716ce7505f3dff37fdbc65b0e7fccf8959ec56bc
Siegfried Goeschl <sgoeschl@apache.org>
2008-01-01 22:55:09 +0000

    Removed unused import statement
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/exec/trunk@607939 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/exec/environment/DefaultProcessingEnvironment.java
+++ src/main/java/org/apache/commons/exec/environment/DefaultProcessingEnvironment.java
@@ -26,7 +26,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Iterator;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
