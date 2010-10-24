d5ecbd3f0af26a7f599ff7a29a9eeedfb5d8b803
Siegfried Goeschl <sgoeschl@apache.org>
2010-10-24 19:54:09 +0000

    Added a simple and standalone test as vehicle to answer questions from the mailing list
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/exec/trunk@1026874 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/exec/StandAloneTest.java
@@ -0,0 +1,42 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.commons.exec;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+
+/**
+ * Placeholder for mailing list question - provided a minimal test case
+ * to answer the question as sel-contained regression test.
+ */
+public class StandAloneTest extends TestCase {
+
+    public void testMe() throws Exception {
+        if(OS.isFamilyUnix()) {
+            File testScript = TestUtil.resolveScriptForOS("./src/test/scripts/standalone");
+            System.out.println("Executing the following test script : " + testScript.getAbsolutePath());
+            Executor exec = new DefaultExecutor();
+            exec.setStreamHandler(new PumpStreamHandler());
+            CommandLine cl = new CommandLine(testScript);
+            exec.execute(cl);
+            assertTrue(new File("./target/mybackup.gz").exists());
+        }        
+    }
+}
