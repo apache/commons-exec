058dc96ffd9f7e7485e44359997d076653cf195d
Siegfried Goeschl <sgoeschl@apache.org>
2016-01-06 10:07:02 +0000

    [EXEC-65] Watchdog can't destroy 'sudo' and 'sleep' - add unit test to ensure that the functionality works
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/exec/trunk@1723260 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/exec/issues/Exec65Test.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.exec.issues;
+
+import org.apache.commons.exec.*;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Test to show that watchdog can destroy 'sudo' and 'sleep'.
+ *
+ * @see <a href="https://issues.apache.org/jira/browse/EXEC-65">EXEC-65</a>
+ */
+public class Exec65Test {
+
+    private static final int TIMEOUT = 3000;
+    private final File testDir = new File("src/test/scripts");
+
+    @Test(expected = ExecuteException.class, timeout = 2*TIMEOUT)
+    public void testExec65WitSleepUsingCommandLine() throws Exception
+    {
+        if(OS.isFamilyUnix())
+        {
+            final DefaultExecutor executor = new DefaultExecutor();
+            executor.setStreamHandler(new PumpStreamHandler(System.out, System.err));
+            final ExecuteWatchdog watchdog = new ExecuteWatchdog(TIMEOUT);
+            executor.setWatchdog(watchdog);
+            final CommandLine command = new CommandLine("sleep");
+            command.addArgument("900");
+
+            executor.execute(command);
+        }
+    }
+
+    @Test(expected = ExecuteException.class, timeout = 2*TIMEOUT)
+    public void testExec65WithSleepUsingShellScript() throws Exception
+    {
+        final DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(new PumpStreamHandler(System.out, System.err));
+        final ExecuteWatchdog watchdog = new ExecuteWatchdog(TIMEOUT);
+        executor.setWatchdog(watchdog);
+        final CommandLine command = new CommandLine(TestUtil.resolveScriptForOS(testDir + "/sleep"));
+
+        executor.execute(command);
+    }
+
+    /**
+     * Please note that this tests make assumptions about the environment. It assumes
+     * that user "root" exists and that the current user is not a "sudoer" already.
+     */
+    @Test(expected = ExecuteException.class, timeout = 2*TIMEOUT)
+    public void testExec65WithSudoUsingShellScript() throws Exception
+    {
+        if(OS.isFamilyUnix())
+        {
+            final DefaultExecutor executor = new DefaultExecutor();
+            executor.setStreamHandler(new PumpStreamHandler(System.out, System.err, System.in));
+            final ExecuteWatchdog watchdog = new ExecuteWatchdog(TIMEOUT);
+            executor.setWatchdog(watchdog);
+            final CommandLine command = new CommandLine(TestUtil.resolveScriptForOS(testDir + "/issues/exec-65"));
+
+            executor.execute(command);
+        }
+    }
+}
