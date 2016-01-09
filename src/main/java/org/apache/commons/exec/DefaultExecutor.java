15bacae628b97b0c3dc785f76b0fd2601a03a9d8
Sebastian Bazley <sebb@apache.org>
2016-01-09 16:48:58 +0000

    EXEC-64  DefaultExecutor swallows IOException cause instead of propagating it (work-round for Java 1.5)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/exec/trunk@1723863 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/exec/DefaultExecutor.java
+++ src/main/java/org/apache/commons/exec/DefaultExecutor.java
@@ -408,7 +408,10 @@ public class DefaultExecutor implements Executor {
                 } catch (final IOException e) {
                     throw e;
                 } catch (final Exception e) {
-                    throw new IOException(e.getMessage());
+                    // Java 1.5 does not support public IOException(String message, Throwable cause)
+                    IOException ioe = new IOException(e.getMessage());
+                    ioe.initCause(e);
+                    throw ioe;
                 }
             }
 
